0f2dbbae9d7b4c60bbf184a13c9b66086d0b2dd7
E. Handerson Obregon Simon <contactosharry1@gmail.com>
2022-12-15 16:33:32 -0500

    Clases Idat

--- Python/Tema.10/Ejercicios/T10.E1.py
+++ Python/Tema.10/Ejercicios/T10.E1.py
@@ -1,21 +1,23 @@
 ""
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
 
 # Ejercicio 1
+
+print("Ingrese los datos necesarios")
 
 # Ingreso de Datos
 
 # Operación
 
-
 def saludo():
 
     print("Hola como estas")
-
 
 # Resultados
 
 saludo()
 
-print("Hi")
+print("Muchas Gracias, programa terminado.")
 
-print("Programa terminado")
