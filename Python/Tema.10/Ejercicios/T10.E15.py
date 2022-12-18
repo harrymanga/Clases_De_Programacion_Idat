2add0c7eb4d6a78a7910c588e49c9e1ed49fd065
E. Handerson Obregon Simon <73681091+harrymanga@users.noreply.github.com>
2022-12-17 23:16:06 -0500

    Clases Idat

--- Python/Tema.10/Ejercicios/T10.E15.py
+++ Python/Tema.10/Ejercicios/T10.E15.py
@@ -9,10 +9,77 @@ print("Ingrese los datos necesarios")
 
 # Funciones
 
+from random import randint
+
+lista = []
+
+
+def generarNum():
+
+    for i in range(10):
+
+        num = randint(10, 100)
+
+        lista.append(num)
+
+        print("Nº", i + 1, "=", num)
+
+    return num
+
+
+def rango(a):
+
+    ran = 0
+
+    for i in lista:
+
+        if 60 <= i <= 70:
+
+            ran += 1
+
+    return ran
+
+
+def múltiplo(b):
+
+    mul = 0
+
+    for i in lista:
+
+        if i % 7 == 0:
+
+            mul += 1
+
+    return mul
+
+
+def mayor(c):
+
+    may = 0
+
+    for i in lista:
+
+        if 80 <= i:
+
+            may += 1
+
+    return may
+
+
 # Ingreso de Datos
 
 # Operación
 
 # Resultados
+
+print("Números Generados")
+
+generarNum()
+
+print("La cantidad de números generados en el rango de 60 a 70 es :", rango(lista))
+
+print("La cantidad de números generados que son múltiplos de 7 son :", múltiplo(lista))
+
+print("La cantidad de números generados mayores a 80 es :", mayor(lista))
 
 print("Muchas Gracias, programa terminado.")
---
+++ Python/Tema.10/Ejercicios/T10.E16.py
@@ -0,0 +1,57 @@
+""
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# Ejercicio 16
+
+print("Ingrese los datos necesarios")
+
+# Funciones
+
+from random import randint
+
+lista = []
+
+
+def generarNum():
+
+    for i in range(5):
+
+        num = randint(0, 20)
+
+        lista.append(num)
+
+        print("Nº", i + 1, "=", num)
+
+    return num
+
+
+def resultado(a):
+
+    b = sum(a) / len(a)
+
+    if 13 <= b:
+
+        res = "El alumno aprobó con : " + str(b)
+
+    else:
+
+        res = "El alumno desaprobó con : " + str(b)
+
+    return res
+
+
+# Ingreso de Datos
+
+# Operación
+
+# Resultados
+
+print("Notas del alumono")
+
+generarNum()
+
+print(resultado(lista))
+
+print("Muchas Gracias, programa terminado.")
