df0eb120df616042c3556ba0f6ae0775c7272e3c
E. Handerson Obregon Simon <73681091+harrymanga@users.noreply.github.com>
2022-12-19 23:02:03 -0500

    Clases Idat

--- Python/Tema.10/Ejercicios/T10.E19.py
+++ Python/Tema.10/Ejercicios/T10.E19.py
@@ -5,4 +5,84 @@
 
 # Ejercicio 19
 
-print("Hola Ju")
+print("Ingrese los datos necesarios")
+
+# Funciones
+
+empleado = {}
+
+
+def registrarDatos():
+
+    while True:
+
+        while True:
+
+            nombre = input("Ingresar el nombre del empleado : ").capitalize()
+
+            if nombre.isalpha():
+
+                break
+
+            else:
+
+                print("Dato invalido, Ingrese el nombre del empleado")
+
+        while True:
+
+            salario = input("Ingrese el monto del salario básico del empleado : ")
+
+            if salario.isalpha() or 0 < float(salario):
+
+                salario = float(salario)
+
+                break
+
+            else:
+
+                print("Dato invalido, Ingrese el salario del empleado")
+
+        while True:
+
+            bonificación = input("Ingrese el monto de la bonificación del empleado : ")
+
+            if bonificación.isalpha() or 0 < float(bonificación):
+
+                bonificación = float(bonificación)
+
+                break
+
+            else:
+
+                print("Dato invalido, Ingrese la bonificación del empleado")
+
+        while True:
+
+            descuento = input("Ingrese el monto del descuento del empleado : ")
+
+            if descuento.isalpha() or 0 < float(descuento):
+
+                descuento = float(descuento)
+
+                break
+
+            else:
+
+                print("Dato invalido, Ingrese el descuento del empleado")
+
+        empleado[nombre] = salario, bonificación, descuento
+
+        break
+
+
+# Ingreso de Datos
+
+# Operación
+
+# Resultados
+
+registrarDatos()
+
+print(empleado)
+
+print("Muchas Gracias, programa terminado.")
